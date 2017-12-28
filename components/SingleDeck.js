c65f8bc62dcc501639e302564e1df96cb61c3486
Trey <treycoopermusic@gmail.com>
2017-12-28 17:03:05 -0500

    refactor: change ternary to function

--- components/SingleDeck.js
+++ components/SingleDeck.js
@@ -4,14 +4,27 @@ import { getDeck  } from '../utils/api';
 import TextButton from './TextButton';
 import { NavigationActions } from 'react-navigation';
 
+function CanStartQuiz (props) {
+  console.log('numCrda',  props)
+  return props.numCards > 0 ? (
+   <TextButton style={{padding: 10}} onPress={() => props.quiz(props.deckTitle)}>
+   Start Quiz
+</TextButton>
+  ) : (<Text>
+  This deck does not have any cards. Please add cards to start a quiz on this subject.
+</Text>)
+ }
+
 export default class SingleDeck extends Component {
   state = {
     deck: {questions: []},
     opacity: new Animated.Value(0)
   }
+
   static navigationOptions = ({navigation}) => ({
     title: navigation.state.params.name,
   });
+
   componentDidMount() {
     const { opacity } = this.state;
     getDeck(this.props.navigation.state.params.name)
@@ -20,6 +33,7 @@ export default class SingleDeck extends Component {
           .start()
         )
   }
+
   goToQuiz = (name) => {
     const { navigate } = this.props.navigation;
     return navigate('Quiz', { name })
@@ -41,7 +55,7 @@ export default class SingleDeck extends Component {
 
   render () {
     const { deck, opacity } = this.state;
-    return deck.questions.length ? (
+    return (
       <Animated.View style={[styles.container, { opacity }]}>
       <Text>
         {deck.title}
@@ -52,26 +66,7 @@ export default class SingleDeck extends Component {
       <TextButton style={{padding: 10}} onPress={() => this.addCard(deck.title)}>
             Add Card
       </TextButton>
-        <TextButton style={{padding: 10}} onPress={() => this.goToQuiz(deck.title)}>
-            Start Quiz
-    </TextButton>
-
-      </Animated.View>
-    )
-    : (
-      <Animated.View style={[styles.container, { opacity }]}>
-      <Text>
-        {deck.title}
-      </Text>
-      <Text>
-        {`${deck.questions.length} ${deck.questions.length === 1 ? 'card' : 'cards'}`}
-      </Text>
-      <TextButton style={{padding: 10}} onPress={() => this.addCard(deck.title)}>
-            Add Card
-      </TextButton>
-        <Text>
-          This deck does not have any cards. Please add cards to start a quiz on this subject.
-        </Text>
+        <CanStartQuiz deckTitle={deck.title} quiz={this.goToQuiz} numCards={deck.questions.length} />
       </Animated.View>
     )
   }
