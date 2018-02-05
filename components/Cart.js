c772869f5d5c53e399f011dd8134c8ad83a5734e
ray <raytorres280@gmail.com>
2018-02-04 22:45:49 -0500

    cart component listitems

--- components/Cart.js
+++ components/Cart.js
@@ -1,46 +1,71 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import TextButton from './TextButton';
-// import { saveDeckTitle } from '../utils/api';
-import { NavigationActions } from 'react-navigation';
-//import SingleDeck from './SingleDeck';
-import io from 'socket.io-client';
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
+import { NavigationActions } from "react-navigation";
+import io from "socket.io-client";
+import axios from "axios";
+import { List, ListItem } from "react-native-elements";
 
-const socket = io('http://localhost:8080');
+const user = { id: 1, email: "rayzorboriqua280@aol.com" };
+
+const socket = io("http://localhost:8080");
 export default class Cart extends Component {
-  state = {
-    text: 'You are not at Smart Mart'
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "hello world",
+      cart: [],
+      order: []
+    };
+  }
 
+  componentWillMount() {
+    axios
+      .get("http://localhost:8080/api/orders/cart/" + user.id)
+      .then(data => this.setState({ cart: data.data.lineItems, order: data.data }));
+  }
   componentDidMount() {
-    socket.on('mobile-cart-update', function(data){
-      console.log('smobile socket working')
-      this.setState({ cart: data })
+    socket.on("mobile-cart-update", function(data) {
+      console.log("smobile socket working");
+      this.setState({ cart: data });
     });
   }
-  
+
   render() {
+    let list = this.state.cart
     return (
-      <View style={styles.container}>
-        <Text style={styles.prompt}>
-          {this.state.text}
-        </Text>
+      <View>
+        <List containerStyle={{ marginBottom: 20 }}>
+        {list.map((l, i) => (
+          <ListItem
+            roundAvatar
+            avatar={{ uri: 'https://cdn.pixabay.com/photo/2014/12/22/00/01/potato-576598_960_720.png' }}
+            key={i}
+            title={l.product.name}
+          />
+        ))}
+      </List>
+      <View>
+        <Text>Subtotal:</Text>
+        <Text>Tax:</Text>
+        <Text>Subtotal</Text>
       </View>
-    )
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 100,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 100
   },
+  item: {},
   prompt: {
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     padding: 15,
     width: 300
-  },
+  }
 });
